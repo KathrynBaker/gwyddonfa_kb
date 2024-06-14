df9eb37d96be0cacdaa14ba9d71ffa6991b8a5e1
kvlb23 <kvlb23@ndw1800.isis.cclrc.ac.uk>
2024-06-14 16:38:52 +0100

    Celebrations when proposed

--- app.js
+++ app.js
@@ -7,6 +7,8 @@ import {createNodeMiddleware} from "@octokit/webhooks";
 import fs from "fs";
 import http from "http";
 
+import {graphql} from "@octokit/graphql";
+
 // This reads your `.env` file and adds the variables from that file to the `process.env` object in Node.js.
 dotenv.config();
 
@@ -30,42 +32,113 @@ const app = new App({
 // This defines the message that your app will post to pull requests.
 const messageForNewPRs = "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
 
-// This adds an event handler that your code will call later. When this event handler is called, it will log the event to the console. Then, it will use GitHub's REST API to add a comment to the pull request that triggered the event.
-async function handlePullRequestOpened({octokit, payload}) {
-  console.log(`Received a pull request event for #${payload.pull_request.number}`);
 
-  try {
-    await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      issue_number: payload.pull_request.number,
-      body: messageForNewPRs,
-      headers: {
-        "x-github-api-version": "2022-11-28",
-      },
-    });
-  } catch (error) {
-    if (error.response) {
-      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`)
+// KVLB Additions
+const query = `query get_info {
+  repository(owner:"KathrynBaker", name:"foxlease-bunting") {
+    issues(last:20, states:CLOSED) {
+      edges {
+        node {
+          title
+          url
+          labels(first:5) {
+            edges {
+              node {
+                name
+              }
+            }
+          }
+        }
+      }
     }
-    console.error(error)
   }
+}
+`;
+
+
+
+const token = process.env.TOKEN;
+const auth = {
+    headers: {
+        authorization: 'token ' + token,
+        Accept: 'application/vnd.github.starfox-preview+json',
+        'Content-Type': 'application/json'
+    }
 };
 
+const niceRequest = async (q, a) => await graphql(q, a);
 
+
+// Event handlers
 async function handleAddedCard({octokit, payload}) {
   console.log(`An item has been added to the project`);
 
 };
 
+var issue_id = "Not set yet";
+
+function something(result) {
+  console.log(`Here is a function, and what it received: `+result);
+};
+
+async function tetherend(result) {
+  console.log(`At the end of my tether`);
+  issue_id = result.repository.issue.id; 
+  console.log(issue_id)
+  
+      var mutation1 = `mutation AddReactionToIssue {
+  addReaction(input:{subjectId:"${issue_id}",content:HOORAY}) {
+    reaction {
+      content
+    }
+    subject {
+      id
+    }
+  }
+}`
+
+    let unhappy = niceRequest(mutation1, auth);
+    
+    console.log(`Aftr the mutation I hope`);
+  };
+
 async function handleLabeledIssue({octokit, payload}) {
   console.log(`The issue #${payload.issue.number} has gained a label: ${payload.label.name}`);
 
+  //let testOne = niceRequest(query, auth);
+  //console.log(testOne);
+  
+  //testOne.then(function(result) {console.log(result)});
+  
+  if (payload.label.name == "proposal") {
+    console.log(`It's a Proposal`);
+    
+    var query1 = `
+    query FindIssueID {
+  repository(owner:"KathrynBaker", name:"foxlease-bunting") {
+    issue(number:${payload.issue.number}) {
+      id
+    }
+  }
+}`
+    //await graphql(query1, auth)
+    let testTwo = niceRequest(query1, auth);
+    //console.log(testTwo);
+    //var test = testTwo.then(data => issue_id);
+    //testTwo.then(function(result) {issue_id = result; console.log(issue_id)});
+    testTwo.then(function(result) {tetherend(result)});
+    //var issue_id = testTwo.then(something(result));//.repository.issue.id;
+    //var issue_id = "Not there yet";
+    //issue_id = testTwo.then(function(result) {return result.repository.issue.id});
+    console.log(`this is the outside one ` + issue_id);
+   
+
+  }  
+  
 };
 
 
 // This sets up a webhook event listener. When your app receives a webhook event from GitHub with a `X-GitHub-Event` header value of `pull_request` and an `action` payload value of `opened`, it calls the `handlePullRequestOpened` event handler that is defined above.
-app.webhooks.on("pull_request.opened", handlePullRequestOpened);
 app.webhooks.on("projects_v2_item.created", handleAddedCard);
 app.webhooks.on("issues.labeled", handleLabeledIssue);
 
